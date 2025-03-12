0f3a6c3187e9525bdb7fa229de2a8b2035931b15
renart117 <renart117@gmail.com>
2025-03-12 13:03:50 +0700

    tutorial selesai

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const menu = document.querySelector('.menu-links');
+  const icon = document.querySelector('.hamberger-icon');
+  menu.classList.toggle('open');
+  icon.classList.toggle('open');
+
+  const about = document.querySelector('#about');
+}
